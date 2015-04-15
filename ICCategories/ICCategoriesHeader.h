bad0bac97adfcd8d7a0d33fb64fdcefae0681293
chenyanjun <chenyj@uulian.com>
2015-04-15 09:26:24 +0800

    - add header.h

---
+++ ICCategories/ICCategoriesHeader.h
@@ -0,0 +1,22 @@
+//
+//  ICCategoriesHeader.h
+//  ICCategories
+//
+//  Created by 陈 颜俊 on 15/4/15.
+//  Copyright (c) 2015年 CHENYANJUN. All rights reserved.
+//
+
+#ifndef ICCategories_ICCategoriesHeader_h
+#define ICCategories_ICCategoriesHeader_h
+
+#import "NSAttributedString+Helper.h"
+#import "NSDate+Helper.h"
+#import "NSManagedObject+Helper.h"
+#import "NSString+Helper.h"
+#import "UINavigationController+Helper.h"
+#import "UIStoryboard+Helper.h"
+#import "UITableViewCell+Helper.h"
+#import "UIView+Helper.h"
+#import "UIViewController+Helper.h"
+
+#endif
